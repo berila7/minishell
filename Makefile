57a659246a598daaa27daf12a13eef0f05eada1f
Mohammed Berila <mberila@e3r1p18.1337.ma>
2025-03-10 14:18:48 +0000

    New Commit

---
+++ Makefile
@@ -0,0 +1,39 @@
+NAME		=		minishell
+
+CC			=		cc
+LIBFT_DIR	=		lib/libft
+PRINTF_DIR	=		lib/ft_printf
+FLAGS		=		-Wall -Wextra -Werror
+LIBFT		=		$(LIBFT_DIR)/libft.a
+PRINTF		=		$(PRINTF_DIR)/libftprintf.a
+
+SRCS		=		src/minishell.c
+OBJS		=		$(SRCS:.c = .o)
+
+all: $(LIBFT_DIR) $(PRINTF_DIR) $(NAME)
+
+$(LIBFT_DIR):
+	@make -C $(LIBFT_DIR)
+
+$(PRINTF_DIR):
+	@make -C $(PRINTF_DIR)
+
+$(NAME): $(OBJS)
+	$(CC) $(FLAGS) $(OBJS) $(LIBFT) $(PRINTF) -o $(NAME)
+
+src/%.o: src/%.c $(LIBFT) $(PRINTF) includes/minishell.h
+	$(CC) $(FLAGS) -c $< -o $@ 
+
+clean:
+	@make -C $(LIBFT_DIR) clean
+	@make -C $(PRINTF_DIR) clean
+	rm -f $(OBJS)
+
+fclean:
+	@make -C $(LIBFT_DIR) fclean
+	@make -C $(PRINTF_DIR) fclean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: $(LIBFT_DIR) $(PRINTF_DIR)
---
+++ lib/libft/ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 15:56:17 by mberila           #+#    #+#             */
+/*   Updated: 2025/02/21 18:09:21 by mberila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int		sign;
+	long	num;
+	long	holder;
+
+	holder = 0;
+	while (*str && ((*str >= 9 && *str <= 13) || *str == 32))
+		str++;
+	sign = 1;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	num = 0;
+	while (ft_isdigit(*str))
+	{
+		num = num * 10 + (*str - '0');
+		if (num < holder)
+			return (-(sign + 1) / 2);
+		holder = num;
+		str++;
+	}
+	return (num * sign);
+}
