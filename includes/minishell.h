9f830168179a66763d90fe738577a57c4777be73
Mohammed Berila <mberila@e3r9p2.1337.ma>
2025-04-08 13:32:31 +0100

    adding tokens struct and token type struct with add token and new token and free token functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 14:12:54 by mberila           #+#    #+#             */
-/*   Updated: 2025/04/08 10:34:55 by mberila          ###   ########.fr       */
+/*   Updated: 2025/04/08 13:00:45 by mberila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,24 @@
 # include <readline/history.h>
 # include "../lib/libft/libft.h"
 # include "../lib/ft_printf/ft_printf.h"
+
+typedef struct s_token t_token;
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,			//	(commands, arguments, filenames)
+    TOKEN_PIPE,			//	|
+    TOKEN_REDIR_IN,		//	<
+    TOKEN_REDIR_OUT,	//	>
+    TOKEN_REDIR_APPEND,	//	>>
+    TOKEN_HEREDOC		//	<<
+}	t_token_type;
+
+struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	t_token			*next;
+};
 
 #endif
--- src/lexer.c
+++ src/lexer.c
@@ -6,9 +6,61 @@
 /*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 11:07:53 by mberila           #+#    #+#             */
-/*   Updated: 2025/04/08 11:09:52 by mberila          ###   ########.fr       */
+/*   Updated: 2025/04/08 13:27:21 by mberila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+t_token	*new_token(char *value, t_token_type type)
+{
+	t_token	*token;
+
+	token = malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->value = value;
+	if (!token->value)
+	{
+		free(token);
+		return (NULL);
+	}
+	token->type = type;
+	token->next = NULL;
+	return (token);
+}
+
+void	add_token(t_token **tokens, t_token *new_token)
+{
+	t_token	*current;
+
+	if (!*tokens)
+	{
+		*tokens = new_token;
+		return ;
+	}
+	current = *tokens;
+	while (current)
+		current = current->next;
+	current->next = new_token;
+}
+
+t_token	*tokenize(char *line)
+{
+	
+}
+
+void	free_tokens(t_token *tokens)
+{
+	t_token	*current;
+	t_token	*next;
+
+	current = tokens;
+	while (current)
+	{
+		next = current->next;
+		free(current->value);
+		free(current);
+		current = next;
+	}
+}
