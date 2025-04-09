614a7717ac2193b79fe7c2c821d5b8f50b98cb89
Mohammed Berila <mberila@e3r9p2.1337.ma>
2025-04-09 11:36:16 +0100

    add the add command function

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -6,9 +6,40 @@
 /*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:31:50 by mberila           #+#    #+#             */
-/*   Updated: 2025/04/09 09:57:09 by mberila          ###   ########.fr       */
+/*   Updated: 2025/04/09 11:35:33 by mberila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+t_command	*new_command(viod)
+{
+	t_command	*cmd;
+
+	
+	
+}
+
+void	add_command(t_command **cmds, t_command *new_cmd)
+{
+	t_command	*current;
+	if (!*cmds)
+	{
+		*cmds = new_cmd;
+		return ;
+	}
+	current = *cmds;
+	while (current->next)
+		current = current->next;
+	current->next = new_cmd;
+}
+
+t_command	*parse_tokens(t_token *tokes)
+{
+	
+}
+
+void	free_commands(t_command *commands)
+{
+	
+}
