fe8b81d57e932ae978ab5cf0d94fdc0946d763a1
berila7 <berilasimo7@gmail.com>
2025-04-16 17:13:09 +0100

    fix escape

--- src/parsing/utils.c
+++ src/parsing/utils.c
@@ -6,7 +6,7 @@
 /*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 17:28:40 by mberila           #+#    #+#             */
-/*   Updated: 2025/04/16 16:27:31 by mberila          ###   ########.fr       */
+/*   Updated: 2025/04/16 16:58:35 by mberila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,51 +35,37 @@ char	*remove_quotes(char *str)
 {
 	int		i;
 	int		j;
-	// int		len;
 	int 	in_single_quote;
 	int		in_double_quote;
 	char	*result;
 
 	i = 0;
 	j = 0;
-	// len = 0;
 	in_single_quote = 0;
 	in_double_quote = 0;
-	// while (str[i])
-	// {
-	// 	if(str[i] == '\'' && !in_double_quote)
-	// 	{
-	// 		if (i > 0 && str[i - 1] == '\\')
-	// 			result[j++] = str[i];
-	// 		in_single_quote = !in_single_quote;
-	// 	}
-	// 	else if (str[i] == '\"' && !in_single_quote)
-	// 		in_double_quote = !in_double_quote;
-	// 	else
-	// 		len++;
-	// 	i++;
-	// }
 	result = malloc(ft_strlen(str) + 1);
 	if (!result)
 		return (NULL);
 	i = 0;
 	while (str[i])
 	{
-		if(str[i] == '\'' && !in_double_quote)
+		if (str[i] == 1)
 		{
-			if (i > 0 && str[i - 1] == '\\')
-				result[j++] = str[i];
+			result[j++] = str[i + 1];
+			i += 2;
+		}
+		else if(str[i] == '\'' && !in_double_quote)
+		{
 			in_single_quote = !in_single_quote;
+			i++;
 		}
 		else if (str[i] == '\"' && !in_single_quote)
 		{
-			if (i > 0 && str[i - 1] == '\\')
-				result[j++] = str[i];
 			in_double_quote = !in_double_quote;
+			i++;
 		}
 		else
-			result[j++] = str[i];
-		i++;
+			result[j++] = str[i++];
 	}
 	result[j] = '\0';
 	return (result);
@@ -160,7 +146,15 @@ char	*remove_escape_chars(char *str)
 		else if (str[i] == '\\' && !escaped && !in_single_quote)
 		{
 			escaped = 1;
-			i++;
+			if (str[i + 1] == '\'' || str[i + 1] == '\"')
+			{
+				result[j++] = 1;
+				result[j++] = str[i + 1];
+				i += 2;
+				escaped = 0;
+			}
+			else
+				i++;
 		}
 		else
 		{
