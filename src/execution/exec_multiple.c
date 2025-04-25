b96df56ec6a409af6d1646fde0f624a226a63c0b
nachat-ayoub <ayoub.nachat.27@gmail.com>
2025-04-25 12:45:05 +0100

    fix handle_redirections name error

--- src/execution/exec_multiple.c
+++ src/execution/exec_multiple.c
@@ -6,7 +6,7 @@
 /*   By: anachat <anachat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 15:40:48 by anachat           #+#    #+#             */
-/*   Updated: 2025/04/25 11:57:34 by anachat          ###   ########.fr       */
+/*   Updated: 2025/04/25 12:44:32 by anachat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int child1(t_cmd *cmd, t_data *data, int *pid)
 			ft_dup2(data->pipe[1], STDOUT_FILENO);
 		}
 
-		if (handle_redilsrections(cmd))
+		if (handle_redirections(cmd))
 			return (1);
 
 		if (!cmd->path)
