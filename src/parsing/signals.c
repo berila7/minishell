49393c6c4fb03fce68c5a519a3e01bf838d6cbc8
berila7 <berilasimo7@gmail.com>
2025-05-01 13:24:05 +0100

    New Commit

--- src/parsing/signals.c
+++ src/parsing/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: berila <berila@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mberila <mberila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 17:28:43 by berila            #+#    #+#             */
-/*   Updated: 2025/04/30 18:21:19 by berila           ###   ########.fr       */
+/*   Updated: 2025/05/01 13:24:04 by mberila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,9 @@ void signal_handler_heredoc(int signum)
     {
         g_sigint_received = 1;
         
-        // Don't close STDIN_FILENO - that's causing our problem!
         
-        // Write a newline for visual clarity
         write(STDOUT_FILENO, "\n", 1);
         
-        // Use readline functions to interrupt the current readline call
         rl_done = 1;               // Tell readline we're done
         rl_replace_line("", 0);    // Clear the line buffer
         rl_on_new_line();          // Move to a new line
@@ -62,22 +59,17 @@ void setup_interactive_signals(void)
 
     signal(SIGQUIT, SIG_IGN);
     
-    // Add this line to properly reset readline's terminal state
     rl_reset_terminal(NULL);
     
-    // Force readline to redisplay the prompt when appropriate
     rl_forced_update_display();
 }
 
 void reset_readline_after_signal(void)
 {
-    // Reset readline's internal state
     rl_reset_after_signal();
     
-    // Make sure cursor is on a fresh line
     rl_on_new_line();
     
-    // Force redisplay of the prompt
     rl_forced_update_display();
 }
 
@@ -114,6 +106,6 @@ void	reset_signals(void)
 
 void reset_readline_signal_handlers(void)
 {
-    rl_catch_signals = 1;  // Let readline handle signals in its own way
-    rl_set_signals();     // Reset readline's signal handlers
+    rl_catch_signals = 1;
+    rl_set_signals();   
 }
