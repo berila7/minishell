7e430f6e31eb278d9ea92c36d6f3e52d146971f0
nachat-ayoub <ayoub.nachat.27@gmail.com>
2025-05-23 14:38:15 +0100

    fix ft_unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anachat <anachat@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 18:26:28 by anachat           #+#    #+#             */
-/*   Updated: 2025/05/19 11:58:14 by anachat          ###   ########.fr       */
+/*   Updated: 2025/05/23 14:37:25 by ayoub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 void	ft_unset(char **args, t_data *data)
 {
-	if (count_args(args) > 1)
-		unset_env(&data->gc, &data->env, args[1]);
+	int	i;
+
+	i = 1;
+	while (args[i])
+	{
+		unset_env(&data->gc, &data->env, args[i]);
+		i++;
+	}
 }
