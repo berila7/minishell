8968b316279605918e1e503ef0c5f4d43f6a124e
nachat-ayoub <ayoub.nachat.27@gmail.com>
2025-06-04 12:25:42 +0100

    update the check leaks script

--- watch_minishell.sh
+++ watch_minishell.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+tput civis  # Hide cursor for a cleaner display
+trap "tput cnorm; exit" SIGINT SIGTERM  # Restore cursor on exit
+
 while true; do
     pid=$(pgrep -x minishell)
+    tput cup 0 0  # Move cursor to top-left
     if [[ -n "$pid" ]]; then
         lsof -p "$pid"
     else
         echo "minishell not running"
     fi
     sleep 1
-    clear
 done
